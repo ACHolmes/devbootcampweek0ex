29c6b54de4fa248b030efbfe41ca009ee1589818
Andrew Holmes <acholmes001@gmail.com>
2021-12-30 16:12:20 +0000

    all tests passed

--- 00_drills.js
+++ 00_drills.js
@@ -56,16 +56,41 @@ const unzip = (arr) => {
 // TODO - write a function which shifts a string `num` characters to the right
 // Example = shiftRight("Hello", 3) === "lloHe"
 const shiftRight = (str, num) => {
-  return 0
   let n = num % str.length;
-  for(let i = 0; i < n; i++){
-
+  let out = "";
+  for (let i = str.length - n ; i < 2* str.length - n ; i++){  
+    out += str[i % str.length]
   }
+  return out
 }
 
 // CHALLENGE - write a function which returns the current date in the following string format:
 // "Today's date is January 7th, 2016. It is 11:37 in the morning."
-const announceDate = () => {}
+
+const announceDate = () => {
+  raw = new Date();
+  let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  let day = raw.getDate();
+  let year = raw.getYear() + 1900;
+  let month = raw.getMonth();
+  let hour = raw.getUTCHours();
+  let minute = raw.getUTCMinutes();
+  let ampm = ''.padStart(2, '0');
+  minute = String(minute).padStart(2, '0');
+  hour = String(hour).padStart(2, '0');
+  if(hour >=12){
+    ampm = 'afternoon';
+  }
+  else{
+    ampm = 'morning';
+  }
+  console.log(day);
+  console.log(year + 1900);
+  console.log(month);
+  console.log(minute);
+  console.log(hour);
+  return `Today's date is ${months[month]} ${day}th, ${year}. It is ${hour}:${minute} in the ${ampm}.`
+}
 
 module.exports = {
   reverse,
--- 05_myReduce.js
+++ 05_myReduce.js
@@ -12,6 +12,18 @@
     -> 20
 */
 
-const myReduce = (arr, f, init) => {}
+/** Assume the description above is incorrect? Should be reduce is the same as fold in ocaml */
+
+const myReduce = (arr, f, init) => {
+  let count = 0;
+  const helper = (arr, f, count) => {
+    if (arr.length != 0){
+      count = f(count, arr[0]);
+      return helper(arr.slice(1,arr.length), f, count)
+    }
+    return count
+  }
+  return helper(arr, f, init)
+}
 
 module.exports = myReduce
